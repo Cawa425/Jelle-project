2d94a34326c73142b0dbd10313b57b2f7cd8e1f1
BANGLADESH228 <55557388+BANGLADESH228@users.noreply.github.com>
2020-08-17 16:28:44 +0500

    Add files via upload

---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject MainMenuUI;
+
+    public void LoadGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("GameScene");
+        Debug.Log("Loading game...");
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    public void LoadSettings()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Settings");
+        Debug.Log("Loading settings...");
+    }
+}
---
+++ Scripts/PauseMenu.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUi;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("Loading MainMenu...");
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    public void LoadSettings()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Settings");
+        Debug.Log("Loading Settings...");
+    }
+}
